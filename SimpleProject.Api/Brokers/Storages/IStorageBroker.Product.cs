80a5d83ae982266673afc7b7393ba97de55f353c
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-03-23 20:15:09 +0300

    BROKER:Storages Insert, SelectAll, Select,Update.Delete

---
+++ SimpleProject.Api/Brokers/Storages/IStorageBroker.Product.cs
@@ -0,0 +1,13 @@
+﻿using SimpleProject.Api.Models.Foundations.Products;
+
+namespace SimpleProject.Api.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<Product> InsertProductAsync(Product product);
+        IQueryable<Product> SelectAllProducts();
+        ValueTask<Product> SelectProductByIdAsync(Guid productId);
+        ValueTask<Product> UpdateProductAsync(Product product);
+        ValueTask<Product> DeleteProductAsync(Product product); 
+    }
+}
--- SimpleProject.Api/Brokers/Storages/StorageBroker.cs
+++ SimpleProject.Api/Brokers/Storages/StorageBroker.cs
@@ -3,10 +3,67 @@
 // Free To Use To Find Comfort And Peace
 //==================================================
 
+using EFxceptions;
+using Microsoft.EntityFrameworkCore;
+
 namespace SimpleProject.Api.Brokers.Storages
 {
-    public class StorageBroker
+    public partial class StorageBroker : EFxceptionsContext
     {
+        private readonly IConfiguration configuration;
+        public StorageBroker(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            Database.Migrate();
+        }
+        public StorageBroker()
+        {
+            Database.EnsureCreated();
+        }
+        private async ValueTask<T> InsertAsync<T>(T @object) where T : class
+        {
+            using var broker = new StorageBroker(this.configuration);
+            broker.Entry(@object).State = EntityState.Added;
+            await broker.SaveChangesAsync();
+
+            return @object;
+        }
+        private IQueryable<T> SelectAll<T>() where T : class
+        {
+            using var broker = new StorageBroker(this.configuration);
+
+            return broker.Set<T>();
+        }
+
+        private async ValueTask<T> SelectAsync<T>(params object[] objectIds) where T : class
+        {
+            using var broker = new StorageBroker(this.configuration);
+
+            return await broker.FindAsync<T>(objectIds);
+        }
+
+        private async ValueTask<T> UpdateAsync<T>(T @object) where T : class
+        {
+            using var broker = new StorageBroker(this.configuration);
+            broker.Entry(@object).State = EntityState.Modified;
+            await broker.SaveChangesAsync();
+
+            return @object;
+        }
+
+        private async ValueTask<T> DeleteAsync<T>(T @object) where T : class
+        {
+            using var broker = new StorageBroker(this.configuration);
+            broker.Entry(@object).State = EntityState.Deleted;
+            await broker.SaveChangesAsync();
+
+            return @object;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-CNTLCPV;Initial Catalog=ProductDb;Integrated Security=True;TrustServerCertificate=True");
+        }
+        public override void Dispose() { }
 
     }
 }
