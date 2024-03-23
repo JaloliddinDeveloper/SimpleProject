fb9febbcf711806151789596828512507cf744ba
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-03-23 19:37:49 +0300

    DATA: Models Add

---
+++ SimpleProject.Api/Brokers/Storages/StorageBroker.cs
@@ -0,0 +1,12 @@
+﻿//==================================================
+// Copyright (c) Coalition Of Good-Hearted Engineers
+// Free To Use To Find Comfort And Peace
+//==================================================
+
+namespace SimpleProject.Api.Brokers.Storages
+{
+    public class StorageBroker
+    {
+
+    }
+}
