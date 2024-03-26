3bde35911805760d1978ff5124fc04d91a9efd2e
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-03-26 22:34:07 +0300

    Add Services

---
+++ SimpleProject.Api/Services/Foundations/Products/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using SimpleProject.Api.Models.Foundations.Products;
+
+namespace SimpleProject.Api.Services.Foundations.Products
+{
+    public interface IProductService
+    {
+        ValueTask<Product> AddProductAsync(Product Product);
+        IQueryable<Product> RetrieveAllProducts();
+        ValueTask<Product> RetrieveProductByIdAsync(Guid ProductId);
+        ValueTask<Product> ModifyProductAsync(Product Product);
+        ValueTask<Product> RemoveProductByIdAsync(Guid ProductId);
+    }
+}
